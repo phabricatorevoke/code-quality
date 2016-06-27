10320fe5eb7d2347693163add8d56cae02b89702
Zama <zamamb@gmail.com>
2016-06-27 15:46:10 +0530

    added sql for getting user details

--- demo-csc-web/src/main/java/demo/csc/customer/dao/CustomerDaoImpl.java
+++ demo-csc-web/src/main/java/demo/csc/customer/dao/CustomerDaoImpl.java
@@ -6,8 +6,12 @@ public class CustomerDaoImpl implements CustomerDao{
 
 	@Override
 	public Customer getUserById(String id) {
+		String sql = "select user_name, email from user where user_id="+id;
 		
 		//Tuned SQL query for better performance, making use of indexes 
+		
+		//Using groupby clause for unique results
+		
 		return null;
 	}
 
